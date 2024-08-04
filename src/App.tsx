74f016f73c9ad9aea2e1fa45c39349afd8accc5c
detalhe <updudu@hotmail.com>
2024-08-04 20:25:56 -0300

    feat: improve App component structure
    - Move App component to separate file for fast refresh support.
    - Update imports in main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { motion, AnimatePresence } from "framer-motion";
+import Layout from "./components/layout/Layout";
+
+const App = () => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        className="App flex justify-center min-h-screen w-full"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}
+      >
+        <Layout />
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { motion, AnimatePresence } from "framer-motion";
-import Layout from "./components/layout/Layout";
-
-const App = () => {
-  return (
-    <AnimatePresence>
-      <motion.div
-        className="App flex justify-center min-h-screen w-full"
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}
-      >
-        <Layout />
-      </motion.div>
-    </AnimatePresence>
-  );
-};
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
